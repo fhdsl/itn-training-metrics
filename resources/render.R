5717fb0b001e8f7026c7ca980d90e69fa1109a18
Kate Isaac <41767733+kweav@users.noreply.github.com>
2025-05-20 16:21:02 -0400

    try message instead of print

--- resources/render.R
+++ resources/render.R
@@ -28,22 +28,22 @@ option_list <- list(
 # Read the results provided as command line argument
 opt_parser <- optparse::OptionParser(option_list = option_list)
 opt <- optparse::parse_args(opt_parser)
-print("got here")
+message("got here")
 jsonResults_loq <- opt$data_in_loq
 jsonResults_loq_supp <- opt$data_in_loq_supp
-print("got here too")
+message("got here too")
 
 # ---------- Interpret the JSON data -----
 
 #Pull the data itself from the API results
 dfloq <- fromJSON(jsonResults_loq)
-print("this good")
+message("this good")
 dfloq_supp <- fromJSON(jsonResults_loq_supp)
-print("this also good")
+message("this also good")
 
 dfloq <- dfloq$results$result$formatted[[2]]
 dfloq_supp <- dfloq_supp$results$result$formatted[[2]]
-print("through here fine")
+message("through here fine")
 
 colnames(dfloq) <- dfloq[1, ] #colnames taken from first row of data
 dfloq <- dfloq[-1, ] #remove the first row of data (original column names)
