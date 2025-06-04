8f6f39f99e7fc7ea772c120be9495402b118fa47
Kate Isaac <41767733+kweav@users.noreply.github.com>
2025-06-04 11:59:23 -0400

    try data fetching

--- resources/render.R
+++ resources/render.R
@@ -22,6 +22,12 @@ option_list <- list(
     type = "character",
     default = NULL,
     help = "Sheet with supplemental Loqui data (json)"
+  ),
+  optparse::make_option(
+    c("--date_in_courses"),
+    type = "character",
+    default = NULL,
+    help = "Sheet with coursera and leanpub course numbers (json)"
   )
 )
 
@@ -30,23 +36,28 @@ opt_parser <- optparse::OptionParser(option_list = option_list)
 opt <- optparse::parse_args(opt_parser)
 jsonResults_loq <- opt$data_in_loq
 jsonResults_loq_supp <- opt$data_in_loq_supp
+jsonResults_courses <- opt$data_in_courses
 
-# ---------- Interpret the JSON data -----
+# ---------- Interpret the JSON data ----- #should functionalize this....
 
 #Pull the data itself from the API results
 dfloq <- fromJSON(jsonResults_loq)
 message("this good")
 dfloq_supp <- fromJSON(jsonResults_loq_supp)
 message("this also good")
+dfcourses <- fromJSON(jsonResults_courses)
 
 dfloq <- dfloq$results$result$formatted[[2]]
 dfloq_supp <- dfloq_supp$results$result$formatted[[2]]
+dfcourses <- dfcourses$results$results$formatted[[2]]
 message("through here fine")
 
 colnames(dfloq) <- dfloq[1, ] #colnames taken from first row of data
 dfloq <- dfloq[-1, ] #remove the first row of data (original column names)
 colnames(dfloq_supp) <- dfloq_supp[1, ]
 dfloq_supp <- dfloq_supp[-1, ]
+colnames(dfcourses) <- dfcourses[1, ]
+dfcourses <- dfcourses[-1, ]
 
 dfloq <- tibble::as_tibble(dfloq)
 dfloq[dfloq==""]<- NA #make no responses NA
@@ -62,9 +73,16 @@ dfloq_supp %<>% drop_na()
 message(dim(dfloq_supp))
 message(colnames(dfloq_supp))
 
+dfcourses <- tibble::as_tibble(dfcourses)
+dfcourses[dfcourses==""]<- NA #make no responses NA
+message(dim(dfcourses))
+message(colnames(dfcourses))
+
+
 sheet_results <- list(
   loqui_data = dfloq,
-  loqui_data_supp = dfloq_supp
+  loqui_data_supp = dfloq_supp,
+  courses_data = dfcourses
 )
 
 rmarkdown::render_site(
